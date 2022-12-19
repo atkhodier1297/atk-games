ddb45d77b836b5f1e82ff0e47a6b8ca45cb2912f
Adam Khodier <khodieradam1297@gmail.com>
2022-12-18 22:19:33 -0500

    working on add button

---
+++ client/src/AddButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddButton() {
+  return (
+    <a class="item">Cart</a>
+  )
+}
+
+export default AddButton
--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -3,7 +3,8 @@ import { useNavigate } from 'react-router-dom'
 import LogoutButton from './LogoutButton'
 import SignupButton from './SignupButton'
 import LoginButton from './LoginButton'
-import StripeCheckout from 'react-stripe-checkout';
+// import StripeCheckout from 'react-stripe-checkout';
+import AddButton from './AddButton'
 
 function Navbar() {
 
@@ -12,11 +13,22 @@ function Navbar() {
     const [showLogin, setShowLogin] = useState(true)
     const [showLogout, setShowLogout] = useState(false)
     const [showSignup, setShowSignup] = useState(true)
+    const [showAdd, setShowAdd] = useState(false)
 
     const currentUser = sessionStorage.getItem("user_id")
 
+    // useEffect(() => {
+    //   if (currentUser.admin===true) {
+    //   setShowAdd(true)
+    //   }
+    //   else {
+    //     console.log("no admin")
+    //   }
+    // },[currentUser])
+
     useEffect(() => {
       if (currentUser) {
+        setShowAdd(true)
         setShowLogin(false)
         setShowLogout(true)
         setShowSignup(false)
@@ -36,24 +48,24 @@ function Navbar() {
       navigate("/products")
     }
 
-    const onToken = (token) => {
+  //   const onToken = (token) => {
 
-      const charge = {
-          token: token.id
-      };
+  //     const charge = {
+  //         token: token.id
+  //     };
   
-      const config = {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ charge: charge, price: price * 100 })
-      };
+  //     const config = {
+  //         method: 'POST',
+  //         headers: {
+  //             'Content-Type': 'application/json'
+  //         },
+  //         body: JSON.stringify({ charge: charge, price: price * 100 })
+  //     };
   
-      fetch(CHARGES_URL, config)
-      .then(res => res.json())
-      .then(console.log)
-  }
+  //     fetch(CHARGES_URL, config)
+  //     .then(res => res.json())
+  //     .then(console.log)
+  // }
 
   return (
     <div class="ui medium top fixed hidden menu">
@@ -67,7 +79,8 @@ function Navbar() {
     <a onClick={navHome} class="active item">Home</a>
     <a onClick={navGames} class="item">Games</a>
     <a class="item">My List</a>
-    <a> <StripeCheckout token={onToken} stripeKey={process.env.REACT_APP_STRIPE_KEY}/></a>
+    {showAdd ? <AddButton/> : null}
+    {/* <a> <StripeCheckout token={onToken} stripeKey={process.env.REACT_APP_STRIPE_KEY}/></a> */}
     <div class="right menu">
       {showLogin ? <LoginButton/> : null}
       {showSignup ? <SignupButton/> : null}
